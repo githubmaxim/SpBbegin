d286cd73966c86bcc15568f88f38100451c32f1e
Maxim <githubmax@ukr.net>
2021-11-05 11:13:48 +0200

    Ничего не помогло. Вернул все в состояние, когда была первая попытка выложить все на HEROKU, оставил только измененным в файле "application.properties" порт с 443 на 5432.

--- src/main/resources/db/migration/V1__init_DB.sql
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -1,73 +1,73 @@
---create table city (
---    id int4 not null,
---    city varchar(255),
---    primary key (id)
---);
---
---create table file_info (
---    id int4 not null,
---    my_key varchar(255),
---    name varchar(255),
---    size bigint,
---    upload_date date,
---    primary key (id)
---);
---
---create table hibernate_sequence (next_val bigint);
---insert into hibernate_sequence values ( 1 );
---
---
---create table logpas (
---    id int4 not null,
---    active bit not null,
---    password varchar(255),
---    username varchar(255),
---    primary key (id)
---);
---
---create table logpass_role (
---    logpass_id int4 not null,
---    roles varchar(255)
---);
---
---create table product (
---    id int4 not null,
---    name varchar(255),
---    price float not null,
---    primary key (id)
---);
---
---create table product3 (
---    id binary(255) not null,
---    name varchar(255),
---    price float not null,
---    primary key (id)
---);
---
---create table university (
---    id int4 not null,
---    name varchar(255),
---    city_id integer,
---    primary key (id)
---);
---
---create table users (
---id int4 not null,
---email varchar(255),
---login varchar(255),
---name varchar(255),
---university_id integer,
---primary key (id)
---);
---
---alter table logpass_role
---    add constraint FKl5i57k1f9w97xx804b6j75g7y
---    foreign key (logpass_id) references logpas (id);
---
---alter table university
---    add constraint FK70cg9iket7hgqy26vs3d652uw
---    foreign key (city_id) references city (id);
---
---alter table users
---    add constraint FKg6bhc1g69lfy3mquw927rmr9m
---    foreign key (university_id) references university (id);
+create table city (
+    id int4 not null,
+    city varchar(255),
+    primary key (id)
+);
+
+create table file_info (
+    id int4 not null,
+    my_key varchar(255),
+    name varchar(255),
+    size bigint,
+    upload_date date,
+    primary key (id)
+);
+
+create table hibernate_sequence (next_val bigint);
+insert into hibernate_sequence values ( 1 );
+
+
+create table logpas (
+    id int4 not null,
+    active bit not null,
+    password varchar(255),
+    username varchar(255),
+    primary key (id)
+);
+
+create table logpass_role (
+    logpass_id int4 not null,
+    roles varchar(255)
+);
+
+create table product (
+    id int4 not null,
+    name varchar(255),
+    price float not null,
+    primary key (id)
+);
+
+create table product3 (
+    id binary(255) not null,
+    name varchar(255),
+    price float not null,
+    primary key (id)
+);
+
+create table university (
+    id int4 not null,
+    name varchar(255),
+    city_id integer,
+    primary key (id)
+);
+
+create table users (
+id int4 not null,
+email varchar(255),
+login varchar(255),
+name varchar(255),
+university_id integer,
+primary key (id)
+);
+
+alter table logpass_role
+    add constraint FKl5i57k1f9w97xx804b6j75g7y
+    foreign key (logpass_id) references logpas (id);
+
+alter table university
+    add constraint FK70cg9iket7hgqy26vs3d652uw
+    foreign key (city_id) references city (id);
+
+alter table users
+    add constraint FKg6bhc1g69lfy3mquw927rmr9m
+    foreign key (university_id) references university (id);
--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
---insert into logpas (id, username, password, active)
---values (1, 'a', 'a', true);
---
---insert into logpass_role (logpass_id, roles)
---values (1, 'ADMIN');
+insert into logpas (id, username, password, active)
+values (1, 'a', 'a', true);
+
+insert into logpass_role (logpass_id, roles)
+values (1, 'ADMIN');
